2589de4cc4b5252fc4d2ab1a924ad26e0141c2af
Anton Rohr <anton.rohr@gmx.de>
2019-04-16 14:29:48 +0200

    added node file

---
+++ src/node.cpp
@@ -0,0 +1,8 @@
+//
+//  node.cpp
+//  neural_net
+//
+//  Created by Anton Rohr on 16.04.19.
+//
+
+#include "node.hpp"
---
+++ src/node.hpp
@@ -0,0 +1,13 @@
+//
+//  node.hpp
+//  neural_net
+//
+//  Created by Anton Rohr on 16.04.19.
+//
+
+#ifndef node_hpp
+#define node_hpp
+
+#include <stdio.h>
+
+#endif /* node_hpp */
